f55098b662f509e50839b28b2207215e5529a9b5
Sander in 't Hout <sander@inthout.me>
2024-04-26 17:23:18 +0200

    Implement querying of CUPS print queues, add log that reports results

--- src/cups_client/client.rs
+++ src/cups_client/client.rs
@@ -2,12 +2,13 @@ use std::{collections::HashMap, io::Cursor};
 use chrono::DateTime;
 use ipp::prelude::*;
 use url::Url;
+use anyhow::{Context, Result};
 
 use crate::config::models::Cups;
 
 use super::models::{*};
 
-fn build_cups_url(cups_settings: &Cups, queue_id: Option<String>) -> String {
+pub fn build_cups_url(cups_settings: &Cups, queue_id: Option<String>) -> String {
     let mut cups_url = Url::parse(&cups_settings.uri).unwrap();
     if !cups_settings.username.is_empty() && !cups_settings.password.is_empty() {
         cups_url.set_username(&cups_settings.username).unwrap();
@@ -34,8 +35,8 @@ fn build_cups_url(cups_settings: &Cups, queue_id: Option<String>) -> String {
 /// ```
 /// send_ipp_request(uri, Operation::ResumePrinter).header().status_code().is_success()
 /// ```
-fn send_ipp_request(uri: String, ignore_tls_errors: bool, op: Operation) -> IppRequestResponse {
-    let uri_p: Uri = uri.parse().unwrap();
+fn send_ipp_request(uri: String, ignore_tls_errors: bool, op: Operation) -> Result<IppRequestResponse> {
+    let uri_p: Uri = uri.parse()?;
     let req = IppRequestResponse::new(
         IppVersion::v1_1(),
         op,
@@ -43,7 +44,7 @@ fn send_ipp_request(uri: String, ignore_tls_errors: bool, op: Operation) -> IppR
     );
     let client = IppClient::builder(uri_p).ignore_tls_errors(ignore_tls_errors).build();
     let resp = client.send(req);
-    resp.unwrap()
+    Ok(resp?)
 }
 
 /// Send an IPP request to do `op` to job `job_id` to the given `uri` and get the response.
@@ -61,8 +62,8 @@ fn send_ipp_request(uri: String, ignore_tls_errors: bool, op: Operation) -> IppR
 /// ```
 /// send_ipp_job_request(uri, Operation::RestartJob, job_id).header().status_code().is_success()
 /// ```
-fn send_ipp_job_request(uri: String, ignore_tls_errors: bool, op: Operation, job_id: i32) -> IppRequestResponse {
-    let uri_p: Uri = uri.parse().unwrap();
+fn send_ipp_job_request(uri: String, ignore_tls_errors: bool, op: Operation, job_id: i32) -> Result<IppRequestResponse> {
+    let uri_p: Uri = uri.parse()?;
     let mut req = IppRequestResponse::new(
         IppVersion::v1_1(),
         op,
@@ -75,15 +76,15 @@ fn send_ipp_job_request(uri: String, ignore_tls_errors: bool, op: Operation, job
 
     let client = IppClient::builder(uri_p).ignore_tls_errors(ignore_tls_errors).build();
     let resp = client.send(req);
-    resp.unwrap()
+    Ok(resp?)
 }
 
-pub fn resume_printer(uri: String, ignore_tls_errors: bool) -> bool {
-    send_ipp_request(uri, ignore_tls_errors, Operation::ResumePrinter).header().status_code().is_success()
+pub fn resume_printer(uri: String, ignore_tls_errors: bool) -> Result<bool> {
+    Ok(send_ipp_request(uri, ignore_tls_errors, Operation::ResumePrinter)?.header().status_code().is_success())
 }
 
-pub fn purge_jobs(uri: String, ignore_tls_errors: bool) -> bool {
-    send_ipp_request(uri, ignore_tls_errors, Operation::PurgeJobs).header().status_code().is_success()
+pub fn purge_jobs(uri: String, ignore_tls_errors: bool) -> Result<bool> {
+    Ok(send_ipp_request(uri, ignore_tls_errors, Operation::PurgeJobs)?.header().status_code().is_success())
 }
 
 pub fn print_job(uri: String, ignore_tls_errors: bool, job_name: String, pdf_data: Vec<u8>) -> bool {
@@ -97,23 +98,23 @@ pub fn print_job(uri: String, ignore_tls_errors: bool, job_name: String, pdf_dat
     resp.unwrap().header().status_code().is_success()
 }
 
-pub fn restart_job(uri: String, ignore_tls_errors: bool, job_id: i32) -> bool {
-    send_ipp_job_request(uri, ignore_tls_errors, Operation::RestartJob, job_id).header().status_code().is_success()
+pub fn restart_job(uri: String, ignore_tls_errors: bool, job_id: i32) -> Result<bool> {
+    Ok(send_ipp_job_request(uri, ignore_tls_errors, Operation::RestartJob, job_id)?.header().status_code().is_success())
 }
 
-pub fn release_job(uri: String, ignore_tls_errors: bool, job_id: i32) -> bool {
-    send_ipp_job_request(uri, ignore_tls_errors, Operation::ReleaseJob, job_id).header().status_code().is_success()
+pub fn release_job(uri: String, ignore_tls_errors: bool, job_id: i32) -> Result<bool> {
+    Ok(send_ipp_job_request(uri, ignore_tls_errors, Operation::ReleaseJob, job_id)?.header().status_code().is_success())
 }
 
-pub fn cancel_job(uri: String, ignore_tls_errors: bool, job_id: i32) -> bool {
-    send_ipp_job_request(uri, ignore_tls_errors, Operation::CancelJob, job_id).header().status_code().is_success()
+pub fn cancel_job(uri: String, ignore_tls_errors: bool, job_id: i32) -> Result<bool> {
+    Ok(send_ipp_job_request(uri, ignore_tls_errors, Operation::CancelJob, job_id)?.header().status_code().is_success())
 }
 
-pub fn get_printers(uri: String, ignore_tls_errors: bool) -> Vec<IppPrinterState> {
+pub fn get_printers(uri: String, ignore_tls_errors: bool) -> Result<Vec<IppPrinterState>> {
     let resp = send_ipp_request(uri.clone(), ignore_tls_errors, Operation::CupsGetPrinters);
     let mut vec: Vec<IppPrinterState> = Vec::new();
 
-    for printer in resp.attributes().groups_of(DelimiterTag::PrinterAttributes) {
+    for printer in resp?.attributes().groups_of(DelimiterTag::PrinterAttributes) {
         let group = printer.attributes().clone();
         let state = group["printer-state"]
             .value()
@@ -128,13 +129,13 @@ pub fn get_printers(uri: String, ignore_tls_errors: bool) -> Vec<IppPrinterState
         vec.push(IppPrinterState { queue_name, description, state, job_count, state_message, state_reason });
     }
 
-    vec
+    Ok(vec)
 }
 
-pub fn get_printer_state(uri: String, ignore_tls_errors: bool) -> IppPrinterState {
-    let resp = send_ipp_request(uri.clone(), ignore_tls_errors, Operation::GetPrinterAttributes);
+pub fn get_printer_state(uri: String, ignore_tls_errors: bool) -> Result<IppPrinterState> {
+    let resp = send_ipp_request(uri.clone(), ignore_tls_errors, Operation::GetPrinterAttributes)?;
 
-    let group = resp.attributes().groups_of(DelimiterTag::PrinterAttributes).next().unwrap();
+    let group = resp.attributes().groups_of(DelimiterTag::PrinterAttributes).next().context("Invalid group returned")?;
     let attributes = group.attributes().clone();
 
     let state = group.attributes()["printer-state"]
@@ -142,32 +143,32 @@ pub fn get_printer_state(uri: String, ignore_tls_errors: bool) -> IppPrinterStat
         .as_enum()
         .and_then(|v| PrinterState::from_i32(*v))
         .unwrap();
-    let job_count = attributes["queued-job-count"].value().as_integer().unwrap().clone();
+    let job_count = attributes["queued-job-count"].value().as_integer().context("Could not parse queued-job-count to i32")?.clone();
     let state_message = attributes["printer-state-message"].value().to_string().clone();
     let queue_name = attributes["printer-name"].value().to_string().clone();
     let description = attributes["printer-info"].value().to_string().clone();
     let state_reason = attributes["printer-state-reasons"].value().to_string().clone();
     //print_attributes(attributes);
-    IppPrinterState { queue_name, description, state, job_count, state_message, state_reason }
+    Ok(IppPrinterState { queue_name, description, state, job_count, state_message, state_reason })
 }
 
-pub fn get_jobs_states(uri: String, ignore_tls_errors: bool) -> Vec<PrintJobState> {
+pub fn get_jobs_states(uri: String, ignore_tls_errors: bool) -> Result<Vec<PrintJobState>> {
     let resp = send_ipp_request(uri.clone(), ignore_tls_errors, Operation::GetJobs);
     let mut vec: Vec<PrintJobState> = Vec::new();
 
-    for job in resp.attributes().groups_of(DelimiterTag::JobAttributes) {
-        let job_id = job.attributes()["job-id"].value().as_integer().unwrap().clone();
-        vec.push(get_job_state(uri.clone(), ignore_tls_errors, job_id));
+    for job in resp?.attributes().groups_of(DelimiterTag::JobAttributes) {
+        let job_id = job.attributes()["job-id"].value().as_integer().context("Could not convert job-id to i32")?.clone();
+        vec.push(get_job_state(uri.clone(), ignore_tls_errors, job_id)?);
     }
 
     // print_attributes(attributes);
-    vec
+    Ok(vec)
 }
 
-fn get_job_state(uri: String, ignore_tls_errors: bool, job_id: i32) -> PrintJobState {
-    let resp = send_ipp_job_request(uri.clone(), ignore_tls_errors, Operation::GetJobAttributes, job_id);
+fn get_job_state(uri: String, ignore_tls_errors: bool, job_id: i32) -> Result<PrintJobState> {
+    let resp = send_ipp_job_request(uri.clone(), ignore_tls_errors, Operation::GetJobAttributes, job_id)?;
 
-    let group = resp.attributes().groups_of(DelimiterTag::JobAttributes).next().unwrap();
+    let group = resp.attributes().groups_of(DelimiterTag::JobAttributes).next().context("Invalid group returned")?;
     let attributes = group.attributes().clone();
 
     // print_attributes(attributes.clone());
@@ -176,20 +177,20 @@ fn get_job_state(uri: String, ignore_tls_errors: bool, job_id: i32) -> PrintJobS
         .value()
         .as_enum()
         .and_then(|v| JobState::from_i32(*v))
-        .unwrap();
+        .context("Could not convert i32 value to JobState")?;
 
-    let creation_time = DateTime::from_timestamp_millis((attributes["time-at-creation"].value().as_integer().unwrap().clone() as i64) * 1000).unwrap();
+    let creation_time = DateTime::from_timestamp_millis((attributes["time-at-creation"].value().as_integer().context("Could not convert time-at-creation to i32")?.clone() as i64) * 1000).unwrap();
 
     // Not every job seems to have a name
     let job_name = if attributes.get_key_value("job-name").is_some() { attributes["job-name"].value().to_string().clone() } else { "".to_string() };
 
-    PrintJobState {
+    Ok(PrintJobState {
         name: job_name,
         id: attributes["job-id"].value().as_integer().unwrap().clone(),
         state: state,
         reason: attributes["job-state-reasons"].value().to_string().clone(),
         created: creation_time,
-    }
+    })
 }
 
 fn print_attributes(attributes: HashMap<String, IppAttribute>) {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,51 @@
-pub mod ipp_client;
+use std::sync::OnceLock;
+
+use anyhow::Result;
+use config::models::Settings;
+use cups_client::models::IppPrinterState;
+use backon::BlockingRetryable;
+use backon::ExponentialBuilder;
+use log::{error, warn, info, debug, trace};
+
+pub mod cups_client;
 pub mod config;
 
-fn main() {
-    let settings = config::loading::load_config();
+pub fn get_settings() -> &'static Settings {
+    static LOG_FILE_REGEX: OnceLock<Settings> = OnceLock::new();
+    LOG_FILE_REGEX.get_or_init(|| config::loading::load_config())
+}
 
-    println!("Hello, world!")
+fn main() {
+    colog::init();
+
+    loop {
+        let cups_print_queues = get_cups_print_queues.retry(&ExponentialBuilder::default().with_factor(4.0)).call();
+        if let Ok(cups_print_queues) = cups_print_queues {
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        } else {
+            failure_wait();
+        }
+    }
+}
+
+fn failure_wait() {
+    error!("Too many failues, waiting 30 seconds before retrying.");
+    std::thread::sleep(std::time::Duration::from_secs(30));
+}
+
+fn get_cups_print_queues() -> Result<Vec<IppPrinterState>> {
+    let settings = get_settings();
+    let url = cups_client::client::build_cups_url(&settings.cups, None);
+    let result = cups_client::client::get_printers(url, settings.cups.ignore_tls_errors);
+
+    match result {
+        Ok(result) => {
+            debug!("CUPS print queues: {:?}", result);
+            Ok(result)
+        }
+        Err(e) => {
+            error!("Error getting CUPS print queues: {:?}", e);
+            Err(e)
+        }
+    }
 }
